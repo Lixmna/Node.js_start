bff2f3aef417066b0229bd1ba978f2db4f70b953
Lixmna <kang7145471@naver.com>
2021-01-19 16:15:43 +0900

    express-session

--- web_server.js
+++ web_server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var router = require('./router/main')(app);
 
 app.set('views', __dirname + '/views');
