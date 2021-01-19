31d4297b2c68f66f885c47ec23a77035902e7c2c
Lixmna <kang7145471@naver.com>
2021-01-19 16:26:11 +0900

    ejs

--- web_server.js
+++ web_server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var router = require('./router/main')(app);
+var fs = require("fs")
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -13,3 +13,14 @@ var server = app.listen(3000,function () {
 })
 
 app.use (express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+var router = require('./router/main')(app, fs);
