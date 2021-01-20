e3ce909d19bde3cf615188a98ac839cf1f0986ed
Lixmna <kang7145471@naver.com>
2021-01-20 19:10:18 +0900

    3001port변경

--- web_server.js
+++ web_server.js
@@ -8,14 +8,17 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000,function () {
-    console.log("Express server has started on port 3000")
+var server = app.listen(3002,function () {
+    console.log("Express server has started on port 3002")
 })
 
-app.use (express.static('public'));
+app.use (cors());
+app.use('/api', api);
 
+app.use (express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 app.use(session({
     secret: '@#@$MYSIGN#@$#$',
     resave: false,
