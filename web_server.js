d328399161af41a9a2ac1951564e9877f1507bd2
Lixmna <kang7145471@naver.com>
2021-01-22 02:43:42 +0900

    key

--- web_server.js
+++ web_server.js
@@ -25,6 +25,7 @@ const client = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
+    key : key,
     logError: true
 });
 
