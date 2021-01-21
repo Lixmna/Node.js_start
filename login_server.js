ea2937d271b45f734825019bbd79fdcb5a0db626
Lixmna <kang7145471@naver.com>
2021-01-22 03:42:44 +0900

    500

--- login_server.js
+++ login_server.js
@@ -25,8 +25,8 @@ mongoose
 require('./config/passport')(passport);
 
 // 라우팅 파트
-app.use('./api/users', users);
+app.use('/api/users', users);
 
-app.listen(3000, (req, res) => {
+app.listen(5000, (req, res) => {
     console.log("서버 실행중..");
 });
